d2fea90068cc5a5db4a379a6543cfa393921ebb1
ans-sna96 <anasyahirul@gmail.com>
2024-08-19 13:24:01 +0700

    change timezone

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -335,7 +335,8 @@ const generatePDF = async (data: any) => {
   }
 
   const printer = new PdfPrinter(fonts)
-  const now = format(new Date(), 'dd/MM/yyyy HH:mm')
+  const now = toZonedTime(new Date(), 'Asia/Jakarta')
+  // const now = format(new Date(), 'dd/MM/yyyy HH:mm')
   const startDate = format(data.startDate, 'dd/MM/yyyy')
   const endDate = format(data.endDate, 'dd/MM/yyyy')
 
